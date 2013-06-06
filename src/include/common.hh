2465e2402a229ad6c68cf00dcfa3d7313007afd8
ppwwyyxx <ppwwyyxxc@gmail.com>
2013-06-06 22:29:32 +0800

    transm coeef

--- src/include/common.hh
+++ src/include/common.hh
@@ -1,5 +1,5 @@
 // File: common.hh
-// Date: Wed Jun 05 20:21:31 2013 +0800
+// Date: Wed Jun 05 20:33:32 2013 +0800
 // Author: Yuxin Wu <ppwwyyxxc@gmail.com>
 
 #pragma once
@@ -21,3 +21,4 @@ const real_t MAX_RECURSIVE_DEPTH = 10;
 const real_t REFL_DIFFUSE_FACTOR = 0.3;
 const real_t REFL_DECAY = 0.2;
 const real_t TRANSM_BLEND_FACTOR = 0.7;
+const real_t REFRACTIVE_INDEX = 1.3;
--- src/include/material/surface.hh
+++ src/include/material/surface.hh
@@ -1,5 +1,5 @@
 // File: surface.hh
-// Date: Sun Apr 07 16:20:13 2013 +0800
+// Date: Wed Jun 05 20:34:13 2013 +0800
 // Author: Yuxin Wu <ppwwyyxxc@gmail.com>
 
 #pragma once
@@ -10,7 +10,8 @@ class Surface {
 	// http://en.wikipedia.org/wiki/Phong_reflection_model
 	public:
 		real_t transparency = 0,
-			   shininess = 0;
+			   shininess = 0,
+			   refractive = 0;
 		Color diffuse = Color::BLACK,
 			  ambient = Color::BLACK,
 			  specular = Color::BLACK;
@@ -19,9 +20,10 @@ class Surface {
 
 		Surface(real_t m_transparency, real_t m_shininess,
 				const Color& m_diffuse, const Color& m_ambient,
-				const Color& m_specular):
+				const Color& m_specular, real_t m_refractive = REFRACTIVE_INDEX):
 			transparency(m_transparency), shininess(m_shininess),
-			diffuse(m_diffuse), ambient(m_ambient), specular(m_specular) {}
+			diffuse(m_diffuse), ambient(m_ambient), specular(m_specular),
+			refractive(m_refractive){}
 
 		static const Surface WHITE_REFL, BLACK_REFL, BLUE_REFL;
 };
